f7f057cfda8eaaf6a7d44a971f98da1ff581e734
Jaap-Willem Dooge <jaap-willem.dooge@outlook.com>
2018-02-10 23:11:24 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+
+namespace YesNoBot
+{
+    class Program
+    {
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("YOUR API CODE HERE");
+
+        static void Main(string[] args)
+        {
+            Bot.OnMessage += BotOnMessageReceived;
+            Bot.StartReceiving();
+            Console.ReadKey();
+            Bot.StopReceiving();
+        }
+
+        private static void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+            if (message == null || message.Type != MessageType.TextMessage) return;
+
+            if (message.Text.StartsWith("/yesno"))
+            {
+                Random rand = new Random();
+                String answer = ((rand.Next(0, 2) == 0) ? "No" : "Yes");
+                Bot.SendTextMessageAsync(message.Chat.Id, answer, ParseMode.Html, false, false, message.MessageId);
+                Console.WriteLine("[Q] " + message.Text);
+                Console.WriteLine("[A] " + answer);
+            }
+        }
+    }
+}
